cd6dcc1107a2ed70482ff62e3837a537631483b3
AwaisYusaf <muhammadawais6640@gmail.com>
2023-02-07 23:01:23 +0500

    Set breakpoint of HomeLayout component

--- components/HomeLayout.tsx
+++ components/HomeLayout.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function HomeLayout({ children }: any) {
     return (
-        <main className="flex flex-col items-center  w-full">
-            <div className="md:w-9/12 w-9/12 flex flex-col items-center">
+        <main className="flex flex-col items-center w-full">
+            <div className="md:w-9/12 w-full flex flex-col items-center">
                 {children}
             </div>
         </main>
